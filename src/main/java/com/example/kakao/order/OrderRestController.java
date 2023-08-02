145392487da05ac9ee301ac55fc9c929c6d7fad0
JS970 <son73097@pusan.ac.kr>
2023-08-02 22:26:20 +0900

    final project

--- src/main/java/com/example/kakao/order/OrderRestController.java
+++ src/main/java/com/example/kakao/order/OrderRestController.java
@@ -2,17 +2,13 @@ package com.example.kakao.order;
 
 import com.example.kakao._core.security.CustomUserDetails;
 import com.example.kakao._core.utils.ApiUtils;
-import com.example.kakao.order.item.Item;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
